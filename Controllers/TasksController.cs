dc961db934050fd37245e79d760c9b413980ad5b
Владимир Тимофеев <energon232@gmail.com>
2026-03-30 22:15:09 +0300

    Change Visual and add functional buttons (delete, add, change)

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -6,13 +6,28 @@ using SharpTask.Services;
 namespace SharpTask.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")] // Путь будет api/tasks
+    [Route("api/tasks")] // Путь будет api/tasks
     public class TasksController : ControllerBase
     {
+        // Внедряем сервис через конструктор (DI)
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
         private readonly ITaskService _taskService;
 
-        // Обновление задачи: PUT api/tasks/5
-        [HttpPut("{id}")]
+        //  Получение задачи по Id
+        [HttpGet("getTaskById/{id}")]
+        public ActionResult<TodoTask> GetById(int id)
+        {
+            var task = _taskService.GetById(id);
+            if (task == null) return NotFound();
+            return task;
+        }
+
+        // Обновление задачи
+        [HttpPut("updateTask/{id}")]
         public IActionResult Update(int id, TodoTask task)
         {
             if (id != task.Id) return BadRequest(); // Защита: ID в пути и в объекте должны совпадать
@@ -23,8 +38,8 @@ namespace SharpTask.Controllers
             return NoContent(); // Стандартный ответ при успешном обновлении (204)
         }
 
-        // Удаление задачи: DELETE api/tasks/5
-        [HttpDelete("{id}")]
+        // Удаление задачи
+        [HttpDelete("deleteTask/{id}")]
         public IActionResult Delete(int id)
         {
             var result = _taskService.Delete(id);
@@ -33,16 +48,12 @@ namespace SharpTask.Controllers
             return NoContent();
         }
 
-        // Внедряем сервис через конструктор (DI)
-        public TasksController(ITaskService taskService)
-        {
-            _taskService = taskService;
-        }
-
-        [HttpGet]
+        // Получение всех задач
+        [HttpGet("getAll")]
         public ActionResult<List<TodoTask>> GetAll() => _taskService.GetAll();
 
-        [HttpPost]
+        // Создание задачи
+        [HttpPost("createTask")]
         public IActionResult Create(TodoTask task)
         {
             _taskService.Add(task);
